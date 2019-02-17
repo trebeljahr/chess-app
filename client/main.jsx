e402acac2222725a2cb57d9876a96b20364d4ae4
trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2019-02-17 10:00:47 +0100

    Add Meteor
    First steps to make this chess-app use a server to enable multiplayer
    functionalities

---
+++ client/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom';
+import ChessApp from '../imports/frontend/src/App.js'
+
+Meteor.startup(() => {
+  render(<ChessApp />, document.getElementById('root'));
+});
