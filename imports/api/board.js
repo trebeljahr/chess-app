d282410bf9e06a2137dda61d0931f2fd3227e98d
trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2019-02-21 20:32:46 +0100

    Remove schema again

--- imports/api/board.js
+++ imports/api/board.js
@@ -3,15 +3,6 @@ import { Mongo } from "meteor/mongo";
 import { check } from "meteor/check";
 
 export const Boards = new Mongo.Collection("boards");
-Boards.schema = new SimpleSchema({
-  board: { type: Array },
-  turn: { type: String },
-  movePart: { type: Number },
-  moveHistory: { type: Array },
-  check: { type: Boolean },
-  checkmate: { type: Boolean },
-  remis: { type: Boolean }
-});
 
 if (Meteor.isServer) {
   // This code only runs on the server
