c948913de31f9750b3789d1a8cb7e690cc41f3de
trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2019-02-21 20:24:24 +0100

    Start implementing simple schema and collection

--- imports/api/board.js
+++ imports/api/board.js
@@ -2,13 +2,25 @@ import { Meteor } from "meteor/meteor";
 import { Mongo } from "meteor/mongo";
 import { check } from "meteor/check";
 
-export const board = new Mongo.Collection("board");
+export const Boards = new Mongo.Collection("boards");
+Boards.schema = new SimpleSchema({
+  board: { type: Array },
+  turn: { type: String },
+  movePart: { type: Number },
+  moveHistory: { type: Array },
+  check: { type: Boolean },
+  checkmate: { type: Boolean },
+  remis: { type: Boolean }
+});
 
 if (Meteor.isServer) {
   // This code only runs on the server
-  Meteor.publish("board", function tasksPublication() {
-    return board.find({
+  Meteor.publish("boards", function tasksPublication() {
+    return Boards.find({
       board: board
     });
   });
 }
+Boards.insert({
+  board: "Hello world!"
+});
