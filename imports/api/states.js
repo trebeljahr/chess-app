035d1ffd048191aa916a77bc6e1794b7592a9112
trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2019-02-22 18:48:21 +0100

    Add a functional withTracker
    The CheckApp component now has access to one static entry in the states database. Now I'll checkout how to use the Meteor.methods to accomplish the state updating functionality.

--- imports/api/states.js
+++ imports/api/states.js
@@ -1,3 +1,13 @@
 import { Mongo } from "meteor/mongo";
 
 export const States = new Mongo.Collection("states");
+
+Meteor.methods({
+  "states.update"({ stateId, fieldsToUpdate }) {
+    const state = States.findOne(stateId);
+
+    States.update(todoId, {
+      $set: { fieldsToUpdate }
+    });
+  }
+});
