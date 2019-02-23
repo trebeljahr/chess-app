9bf6232f8ffe5873fdca1b50b9f43facdfb4b4e7
Nrin <Nrin3141@users.noreply.github.com>
2019-02-23 14:28:16 +0100

    Add tools, update tracker, add new-game  method

--- imports/frontend/src/App.js
+++ imports/frontend/src/App.js
@@ -1,19 +1,18 @@
 import React from "react";
 import "./App.css";
 import { getDefaultState } from "./helpers/getDefaultState.js";
-import { createFieldMarkers } from "./tileMarkers/createFieldMarkers.js";
-import { changeTurns } from "./helpers/changeTurns.js";
-import { invertColor } from "./helpers/invertColor.js";
-import { findFigures } from "./helpers/findFigures.js";
 import { convertPos } from "./helpers/convertPos.js";
+import { createFieldMarkers } from "./tileMarkers/createFieldMarkers.js";
 import { validateMove } from "./helpers/validateMove.js";
 import { checkForCheck } from "./gameFunctions/checkForCheck.js";
-import { updateBoard, removeMarkers } from "./gameFunctions/updateBoard.js";
+import { updateBoard } from "./gameFunctions/updateBoard.js";
+import { changeTurns } from "./helpers/changeTurns.js";
 import { checkForCheckMate, checkForRemis } from "./gameFunctions/endGame.js";
 import { RevertLastMoveInstructions } from "./helpers/RevertLastMoveInstructions.js";
-import { checkForMovedKing } from "./helpers/movedRochadeFigures.js";
+import { invertColor } from "./helpers/invertColor.js";
 import Board from "./components/Board";
 import Dashboard from "./components/Dashboard";
+import { checkForMovedKing } from "./helpers/movedRochadeFigures.js";
 
 import { withTracker } from "meteor/react-meteor-data";
 import { States } from "../../api/states.js";
@@ -25,15 +24,18 @@ class ChessApp extends React.Component {
   }
   handleClick = field => {
     let { row, col } = convertPos(field);
-    let figure = this.state.board[row][col].figure;
+    let fieldContent = this.state.board[row][col];
     if (this.state.movePart === 1) {
-      if (figure.color === this.state.figure.color) {
+      if (fieldContent.figure.color === this.state.figure.color) {
+        if (row === this.state.oldPos.row && col === this.state.oldPos.col) {
+          this.setState(state => {});
+        }
         this.setState(state => {
           return {
             board: createFieldMarkers(state.board, row, col, "valid"),
             movePart: 1,
             oldPos: { row, col },
-            figure
+            figure: fieldContent.figure
           };
         });
       } else if (validateMove(this.state.board, row, col)) {
@@ -41,46 +43,42 @@ class ChessApp extends React.Component {
         let move = {
           figure: this.state.figure,
           oldPos: this.state.oldPos,
-          newPos,
-          secondFigure: figure
+          newPos: newPos
         };
-        this.setState(state => {
-          return {
-            board: updateBoard(state.board, move),
-            turn: changeTurns(state.turn),
-            check: checkForCheck(state.board, state.turn),
-            movePart: 0,
-            checkmate: checkForCheckMate(state.board, state.turn),
-            remis:
-              !checkForCheckMate(state.board, state.turn) &&
-              checkForRemis(state.board, state.turn)
-                ? true
-                : false,
-            moveHistory: [...state.moveHistory, move]
-          };
+        let moveHistoryCopy = [...this.state.moveHistory];
+        moveHistoryCopy.push(move);
+        if (
+          checkForCheck(this.state.board, this.state.turn) &&
+          this.state.check !== true
+        ) {
+          this.setState({ check: true });
+        }
+        this.setState({
+          board: updateBoard(this.state.board, move),
+          turn: changeTurns(this.state.turn),
+          movePart: 0,
+          moveHistory: moveHistoryCopy
         });
-      } else {
-        this.setState(state => {
-          return {
-            board: removeMarkers(state.board, ["valid", "selected"]),
-            movePart: 0
-          };
-        });
+        if (checkForCheckMate(this.state.board, this.state.turn)) {
+          this.setState({ checkmate: true });
+        }
+        if (checkForRemis(this.state.board, this.state.turn)) {
+          this.setState({ remis: true });
+        }
       }
-    } else if (figure.color === this.state.turn) {
+    } else if (fieldContent.figure.color === this.state.turn) {
       this.setState({
         board: createFieldMarkers(this.state.board, row, col, "valid"),
         movePart: 1,
         oldPos: { row, col },
-        figure
+        figure: fieldContent.figure
       });
     }
   };
-
   handleUndo = () => {
     let move = RevertLastMoveInstructions(this.state.moveHistory);
     this.setState({
-      board: updateBoard(this.state.board, move, false, true),
+      board: updateBoard(this.state.board, move),
       turn: changeTurns(this.state.turn),
       movePart: 0,
       check: checkForCheck(this.state.board, this.state.turn),
@@ -88,34 +86,47 @@ class ChessApp extends React.Component {
       remis: false
     });
   };
-  resetBoard = () => {
-    Meteor.call("states.update");
+  handleNewGame = () => {
+    if (!!States.find({ id: "test-game" }).fetch()[0]) return;
+    Meteor.call("states.createNew", "test-game");
   };
-
+  resetBoard = () => {
+    this.setState(getDefaultState());
+  };
   render() {
-    return this.props.states.map(state => (
-      <div key={state._id}>
-        <Board
-          board={state.board}
-          turnAround={state.turn === "black"}
+    return (
+      <div>
+        {this.props.game ? (
+          <Board
+            //turnAround={this.state.turn === "black"}
+            board={this.props.game.board}
+            handleClick={this.handleClick}
+          />
+        ) : null}
+
+        {/*<Board
+          //turnAround={this.state.turn === "black"}
+          board={this.state.board}
           handleClick={this.handleClick}
         />
         <Dashboard
-          checkmate={state.checkmate}
-          remis={state.remis}
-          turn={state.turn}
-        />
-        <ResetBoard resetBoard={this.resetBoard} />
-        <UndoButton
+          checkmate={this.state.checkmate}
+          remis={this.state.remis}
+          turn={this.state.turn}
+          resetBoard={this.resetBoard}
           handleUndo={this.handleUndo}
-          moveHistory={state.moveHistory}
-        />
+          moveHistory={this.state.moveHistory}
+        />*/}
+        <button onClick={this.handleNewGame}>Add a new game!</button>
       </div>
-    ));
+    );
   }
 }
-
-export default withTracker(({ id }) => {
-  Meteor.subscribe("states.public");
-  return { states: States.find({ _id: "zZdYxLYtTnWZYJ9Wh" }).fetch() };
+const ChessAppContainer = withTracker(({}) => {
+  let game = States.find({}).fetch()[0];
+  //console.log(game);
+  return {
+    game
+  };
 })(ChessApp);
+export default ChessAppContainer;
