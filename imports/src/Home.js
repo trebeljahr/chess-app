fe79d2637df271ffec876b202ca2654cd49e4436
trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2019-02-23 23:22:44 +0100

    Add styling, fix undo function

--- imports/src/Home.js
+++ imports/src/Home.js
@@ -24,25 +24,31 @@ class Home extends React.Component {
     return (
       <div>
         {this.props.states ? (
-          <div>
+          <div className="centerFlex">
+            Create a game
             <form onSubmit={this.createNewGame}>
-              <input type="text" name="name" placeholder="Name your game" />
+              <input type="text" name="name" placeholder="Name" />
               <input type="submit" />
             </form>
-            {this.props.states.map(state => (
-              <div key={state._id} className="gameContainer">
-                <h3>{state.name}</h3>
-                <a className="linkButton" href={"/games/" + state._id}>
-                  Join
-                </a>
-                <button
-                  className="deleteButton"
-                  onClick={() => this.handleDelete(state._id)}
-                >
-                  Delete!
-                </button>
-              </div>
-            ))}
+            <h2>Join a game</h2>
+            <div className="game-postings-container">
+              {this.props.states.map(state => (
+                <div key={state._id} className="game-posting">
+                  <h3 className="game-posting-title">{state.name}</h3>
+                  <div className="game-posting-controls">
+                    <a className="linkButton" href={"/games/" + state._id}>
+                      Join
+                    </a>
+                    <button
+                      className="deleteButton"
+                      onClick={() => this.handleDelete(state._id)}
+                    >
+                      Delete!
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         ) : (
           <div>Loading...</div>
