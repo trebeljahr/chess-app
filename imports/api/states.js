7935eb87c5b44724df679c6505961a7db16f566a
trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2019-02-23 14:28:16 +0100

    Add tools, update tracker, add new-game  method

--- imports/api/states.js
+++ imports/api/states.js
@@ -1,13 +1,17 @@
 import { Mongo } from "meteor/mongo";
-
+import { getDefaultState } from "../frontend/src/helpers/getDefaultState.js";
 export const States = new Mongo.Collection("states");
 
 Meteor.methods({
+  "states.createNew"(id) {
+    States.insert({ id, ...getDefaultState() });
+  },
   "states.update"({ stateId, fieldsToUpdate }) {
     const state = States.findOne(stateId);
 
-    States.update(todoId, {
-      $set: { fieldsToUpdate }
-    });
+    States.update(stateId, fieldsToUpdate);
+  },
+  "states.list"() {
+    return getDefaultState();
   }
 });
--- server/main.js
+++ server/main.js
@@ -1,5 +1,1 @@
 import "../imports/api/states.js";
-
-Meteor.publish("states.public", function(id) {
-  return States.find();
-});
