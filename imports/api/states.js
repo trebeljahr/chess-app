4e2b52fc6ccd735093554c93927cd7cc5f78bb33
trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2019-02-26 15:26:37 +0100

    Add fancy alerts & style addGame box
    Also added automatic redirection after creating a game.

--- imports/api/states.js
+++ imports/api/states.js
@@ -5,6 +5,7 @@ export const States = new Mongo.Collection("states");
 Meteor.methods({
   "states.createNew"({ name }) {
     States.insert({ name, ...getDefaultState() });
+    return States.findOne({ name });
   },
   "states.update"({ _id, fieldsToUpdate }) {
     States.update(_id, { $set: { ...fieldsToUpdate } });
