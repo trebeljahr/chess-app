7d09bf21994f2afd6a1cbff3248b3e79ec9404ea
Nrin <Nrin3141@users.noreply.github.com>
2019-03-01 07:34:32 +0100

    Add non-functional chat component and style it

--- imports/src/components/UndoButton/index.js
+++ imports/src/components/UndoButton/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const UndoButton = props => {
   return props.moveHistory.length >= 1 ? (
-    <button onClick={() => props.handleUndo()}>Undo!</button>
+    <button onClick={() => props.handleUndo()}>⤾</button>
   ) : null;
 };
 export default UndoButton;
