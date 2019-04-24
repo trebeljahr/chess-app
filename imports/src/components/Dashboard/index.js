96ce504f381fdf4e6df12768a975de849872b88f
Nrin <Nrin3141@users.noreply.github.com>
2019-04-24 21:49:23 +0200

    Add styled-jsx

--- imports/src/components/Dashboard/index.js
+++ imports/src/components/Dashboard/index.js
@@ -1,6 +1,4 @@
 import React from "react";
-import "./Dashboard.css";
-import { invertColor } from "../../helpers/invertColor.js";
 import UndoButton from "../UndoButton";
 import ChatContainer from "../Chat";
 import TextContainer from "../TextContainer";
@@ -11,24 +9,22 @@ const Dashboard = game => {
   return (
     <div className="Dashboard">
       <MoveHistory moveHistory={game.moveHistory} />
-      <div className="controlElements">
-        <TextContainer
-          color={game.color}
-          checkmate={game.checkmate}
-          turn={game.turn}
-          remis={game.remis}
-        />
-        <HomeButton />
-        <UndoButton
-          color={game.color}
-          proposeUndo={game.proposeUndo}
-          revertUndoProposal={game.revertUndoProposal}
-          handleUndo={game.handleUndo}
-          moveHistory={game.moveHistory}
-          offerTakeback={game.offerTakeback}
-        />
-        <ChatContainer _id={game._id} messages={game.messages} />
-      </div>
+      <TextContainer
+        color={game.color}
+        checkmate={game.checkmate}
+        turn={game.turn}
+        remis={game.remis}
+      />
+      <HomeButton />
+      <UndoButton
+        color={game.color}
+        proposeUndo={game.proposeUndo}
+        revertUndoProposal={game.revertUndoProposal}
+        handleUndo={game.handleUndo}
+        moveHistory={game.moveHistory}
+        offerTakeback={game.offerTakeback}
+      />
+      <ChatContainer _id={game._id} messages={game.messages} />
     </div>
   );
 };
