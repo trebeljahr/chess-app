f64a74eb517c5358f61d426813b5ab841654a68a
Nrin <Nrin3141@users.noreply.github.com>
2019-04-26 20:48:49 +0200

    Work on rerouting and login page

--- imports/src/components/Login/index.js
+++ imports/src/components/Login/index.js
@@ -16,7 +16,61 @@ export default class Navbar extends Component {
   render() {
     return (
       <div>
-        <div ref="container" />
+        <div className="banner">
+          <h1>Nrin's Chess</h1>
+          <h2>Play chess against your friends!</h2>
+        </div>
+        <div ref="container" className="login" />
+        <style jsx>{`
+          .login {
+            position: fixed;
+            top: 0;
+            right: 0;
+            z-index: 2;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            width: 40vw;
+            padding: 5vmin;
+            height: 100vh;
+            background: white;
+            overflow: scroll;
+          }
+          .banner {
+            color: white;
+            position: fixed;
+            top: 0;
+            left: 0;
+            z-index: 2;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            text-align: center;
+            width: 60vw;
+            height: 100vh;
+            background: linear-gradient(
+              111.45349766483548deg,
+              rgba(2, 27, 62, 1) 5.73779698099772%,
+              rgba(81, 119, 149, 1) 96.25816012540469%
+            );
+          }
+          h1 {
+            font-weight: bold;
+          }
+          @media only screen and (max-width: 600px) {
+            .banner {
+              width: 100vw;
+              height: 30vh;
+            }
+            .login {
+              top: 30vh;
+              left: 0;
+              width: 100vw;
+              height: 70vh;
+            }
+          }
+        `}</style>
       </div>
     );
   }
