b8473dd3bb2f95168bc6c116e5f80c8043b1d9f3
trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2019-04-27 13:04:47 +0200

    Add timestamp to createdGames

---
+++ imports/src/components/GameListings/GameCreationTime.js
@@ -0,0 +1,25 @@
+import React from "react";
+class GameCreationTime extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      now: Math.floor((Date.now() - this.props.timestamp) / 60000),
+      interval: false
+    };
+  }
+  tick = () => {
+    this.setState({
+      now: Math.floor((Date.now() - this.props.timestamp) / 60000)
+    });
+  };
+  componentDidMount = () => {
+    this.setState({ interval: setInterval(this.tick, 60000) });
+  };
+  componentWillUnmount = () => {
+    clearInterval(this.state.interval);
+  };
+  render() {
+    return <div>created {this.state.now} minutes ago</div>;
+  }
+}
+export default GameCreationTime;
