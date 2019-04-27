9f7ae2ea67735a0679b0465fc5a823b729abd3ba
trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2019-04-27 14:22:58 +0200

    Add responsive games display grid

--- imports/src/components/GameListings/index.js
+++ imports/src/components/GameListings/index.js
@@ -23,14 +23,15 @@ const GameListings = ({ games, handleJoin, handleDelete }) => (
       }
       .gpc {
         position: relative;
-        width: 100%;
+        width: 100vw;
         height: 90vh;
         margin: 0;
-        padding: 0;
+        padding: 0 10vw;
         padding-top: 5px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        grid-auto-columns: min-content min-content min-content;
+        grid-auto-rows: max-content;
         overflow: scroll;
         scrollbar-color: #286090 #fff;
       }
@@ -44,21 +45,57 @@ const GameListings = ({ games, handleJoin, handleDelete }) => (
         background: #286090;
       }
       .game-posting {
-        width: 80vw;
-        border: 1px solid;
-        margin: 4px;
         display: flex;
+        padding: 5px;
         flex-direction: column;
         text-align: center;
         justify-content: center;
         align-items: center;
+        background: #ccd0d4;
       }
       .game-posting-title {
+        max-width: 25vw;
         margin-left: 2%;
         text-align: left;
         white-space: nowrap;
         overflow: hidden;
         text-overflow: ellipsis;
+        font-weight: bold;
+      }
+      @media only screen and (min-width: 801px) {
+        .gpc div:nth-child(odd) {
+          background: white;
+        }
+      }
+      @media only screen and (max-width: 800px) {
+        .game-posting-title {
+          max-width: 40vw;
+        }
+        .gpc {
+          grid-template-columns: 1fr 1fr;
+          grid-auto-columns: min-content min-content;
+        }
+        .gpc div:nth-child(4n-1) {
+          background: white;
+        }
+        .gpc div:nth-child(4n-2) {
+          background: white;
+        }
+      }
+      @media only screen and (max-width: 600px) {
+        .game-posting-title {
+          max-width: 80vw;
+        }
+        .gpc {
+          grid-template-columns: 1fr;
+          grid-auto-columns: min-content;
+        }
+        .gpc div:nth-child(odd) {
+          background: white;
+        }
+        .gpc div:nth-child(4n-2) {
+          background: #ccd0d4;
+        }
       }
     `}</style>
   </div>
